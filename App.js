1d864aec73c96e218ece58a9e9c1b9b2cd38f317
Lucas Oliveira <olivmath@protonmail.com>
2024-07-07 01:07:25 -0300

    add: splash screen

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen.js';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 const Stack = createStackNavigator();
 
+// Previne que a splash screen desapareça automaticamente
+SplashScreen.preventAutoHideAsync();
+
+
 export default function App() {
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
+
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">

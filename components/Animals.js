1ef96a4a2b641dc54a3d72d84c0cd9cdcb0edc7f
Lucas Oliveira <olivmath@protonmail.com>
2024-07-07 01:51:50 -0300

    add: animals metadata

--- components/Animals.js
+++ components/Animals.js
@@ -11,96 +11,150 @@ export const Animals = [
     ],
     sound: false,
   },
-
-  // {
-  //   id: 2,
-  //   name: "Garça",
-  //   description: "A garça é uma ave elegante, frequentemente encontrada em áreas úmidas, conhecida por seu longo pescoço e pernas.",
-  //   image: require("../assets/animals/garca.jpg"),
-  //   sound: require("../assets/animals/garca.mp3"),
-  // },
-  // {
-  //   id: 3,
-  //   name: "Biguá",
-  //   description: "O biguá é uma ave aquática excelente mergulhadora, que se alimenta principalmente de peixes.",
-  //   image: require("../assets/animals/bigua.jpg"),
-  //   sound: require("../assets/animals/bigua.mp3"),
-  // },
-  // {
-  //   id: 4,
-  //   name: "Socozinho",
-  //   description: "O socozinho é uma pequena ave de hábitos solitários, encontrada em margens de rios e lagoas.",
-  //   image: require("../assets/animals/socozinho.jpg"),
-  //   sound: require("../assets/animals/socozinho.mp3"),
-  // },
+  {
+    id: 2,
+    name: "Garça",
+    description:
+      "A garça é uma ave elegante, frequentemente encontrada em áreas úmidas, conhecida por seu longo pescoço e pernas.",
+    images: [
+      require("../assets/animals/garca/1.jpg"),
+      require("../assets/animals/garca/2.jpg"),
+      require("../assets/animals/garca/3.jpg"),
+    ],
+    sound: false,
+  },
+  {
+    id: 3,
+    name: "Biguá",
+    description:
+      "O biguá é uma ave aquática excelente mergulhadora, que se alimenta principalmente de peixes.",
+    images: [
+      require("../assets/animals/bigua/1.jpg"),
+      require("../assets/animals/bigua/2.jpg"),
+      require("../assets/animals/bigua/3.jpg"),
+    ],
+    sound: false,
+  },
+  {
+    id: 4,
+    name: "Socozinho",
+    description:
+      "O socozinho é uma pequena ave de hábitos solitários, encontrada em margens de rios e lagoas.",
+    images: [
+      require("../assets/animals/socozinho/1.jpg"),
+      require("../assets/animals/socozinho/2.jpg"),
+      require("../assets/animals/socozinho/3.jpg"),
+    ],
+    sound: false,
+  },
   // {
   //   id: 5,
   //   name: "Lavadeira-Mascarada",
   //   description: "A lavadeira-mascarada é uma ave pequena e ativa, conhecida por sua plumagem branca com uma máscara preta ao redor dos olhos.",
-  //   image: require("../assets/animals/lavadeira-mascarada.jpg"),
-  //   sound: require("../assets/animals/lavadeira-mascarada.mp3"),
+  //   images: [
+  //  require("../assets/animals/lavadeira-mascarada/1.jpg"),
+  //  require("../assets/animals/lavadeira-mascarada/2.jpg"),
+  //  require("../assets/animals/lavadeira-mascarada/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 6,
   //   name: "Frango-d'água",
   //   description: "O frango-d'água é uma ave aquática que vive em pântanos e áreas alagadas, facilmente reconhecível pelo seu comportamento furtivo.",
-  //   image: require("../assets/animals/frango-dagua.jpg"),
-  //   sound: require("../assets/animals/frango-dagua.mp3"),
+  //   images: [
+  //  require("../assets/animals/frango-dagua/1.jpg"),
+  //  require("../assets/animals/frango-dagua/2.jpg"),
+  //  require("../assets/animals/frango-dagua/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 7,
   //   name: "Pinguim-de-Magalhães",
   //   description: "O pinguim-de-magalhães é uma espécie de pinguim encontrada na América do Sul, conhecida por suas colônias numerosas.",
-  //   image: require("../assets/animals/pinguim-de-magalhaes.jpg"),
-  //   sound: require("../assets/animals/pinguim-de-magalhaes.mp3"),
+  //   images: [
+  //  require("../assets/animals/pinguim-de-magalhaes/1.jpg"),
+  //  require("../assets/animals/pinguim-de-magalhaes/2.jpg"),
+  //  require("../assets/animals/pinguim-de-magalhaes/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 8,
   //   name: "Pernilongo-de-costas-brancas",
   //   description: "O pernilongo-de-costas-brancas é uma ave pernalta que se alimenta de pequenos invertebrados em águas rasas.",
-  //   image: require("../assets/animals/pernilongo-de-costas-brancas.jpg"),
-  //   sound: require("../assets/animals/pernilongo-de-costas-brancas.mp3"),
+  //   images: [
+  //  require("../assets/animals/pernilongo-de-costas-brancas/1.jpg"),
+  //  require("../assets/animals/pernilongo-de-costas-brancas/2.jpg"),
+  //  require("../assets/animals/pernilongo-de-costas-brancas/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 9,
   //   name: "Gavião-caramujeiro",
   //   description: "O gavião-caramujeiro é uma ave de rapina que se alimenta quase exclusivamente de caramujos aquáticos.",
-  //   image: require("../assets/animals/gaviao-caramujeiro.jpg"),
-  //   sound: require("../assets/animals/gaviao-caramujeiro.mp3"),
+  //   images: [
+  //  require("../assets/animals/gaviao-caramujeiro/1.jpg"),
+  //  require("../assets/animals/gaviao-caramujeiro/2.jpg"),
+  //  require("../assets/animals/gaviao-caramujeiro/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 10,
   //   name: "Gavião-do-mangue",
   //   description: "O gavião-do-mangue é uma ave de rapina que habita áreas de mangue e se alimenta de peixes e pequenos invertebrados.",
-  //   image: require("../assets/animals/gaviao-do-mangue.jpg"),
-  //   sound: require("../assets/animals/gaviao-do-mangue.mp3"),
+  //   images: [
+  //  require("../assets/animals/gaviao-do-mangue/1.jpg"),
+  //  require("../assets/animals/gaviao-do-mangue/2.jpg"),
+  //  require("../assets/animals/gaviao-do-mangue/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 11,
   //   name: "Japim-preto",
   //   description: "O japim-preto é uma ave conhecida por sua plumagem preta e comportamento social complexo, vivendo em grupos.",
-  //   image: require("../assets/animals/japim-preto.jpg"),
-  //   sound: require("../assets/animals/japim-preto.mp3"),
+  //   images: [
+  //  require("../assets/animals/japim-preto/1.jpg"),
+  //  require("../assets/animals/japim-preto/2.jpg"),
+  //  require("../assets/animals/japim-preto/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 12,
   //   name: "Narceja",
   //   description: "A narceja é uma ave de hábitos migratórios, encontrada em áreas úmidas, com plumagem camuflada que a ajuda a se esconder.",
-  //   image: require("../assets/animals/narceja.jpg"),
-  //   sound: require("../assets/animals/narceja.mp3"),
+  //   images: [
+  //  require("../assets/animals/narceja/1.jpg"),
+  //  require("../assets/animals/narceja/2.jpg"),
+  //  require("../assets/animals/narceja/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 13,
   //   name: "Atobá",
   //   description: "O atobá é uma ave marinha com grande habilidade de mergulho, conhecida por suas plumagens contrastantes.",
-  //   image: require("../assets/animals/atoba.jpg"),
-  //   sound: require("../assets/animals/atoba.mp3"),
+  //   images: [
+  //  require("../assets/animals/atoba/1.jpg"),
+  //  require("../assets/animals/atoba/2.jpg"),
+  //  require("../assets/animals/atoba/3.jpg")
+  // ],
+  //   sound: false,
   // },
   // {
   //   id: 14,
   //   name: "Tuiuiú",
   //   description: "O tuiuiú é a maior ave voadora da América do Sul, conhecida por seu pescoço preto e plumagem branca.",
-  //   image: require("../assets/animals/tuiuiu.jpg"),
-  //   sound: require("../assets/animals/tuiuiu.mp3"),
+  //   images: [
+  //  require("../assets/animals/tuiuiu/1.jpg"),
+  //  require("../assets/animals/tuiuiu/2.jpg"),
+  //  require("../assets/animals/tuiuiu/3.jpg")
+  // ],
+  //   sound: false,
   // }
 ];
